4f14d59f6d052b738315b571c8df3d71348268c1
Shubham <shubham@example.com>
2026-05-17 16:15:56 +0530

    Fixed LangChain text splitter import

--- rag_using_langchain.py
+++ rag_using_langchain.py
@@ -1,6 +1,6 @@
 import os
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import PromptTemplate
